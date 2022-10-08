9bacc5c92d861a8e2ff2f47a51ad37944c983f47
glazialuna <glaziawy@gmail.com>
2022-10-08 11:54:07 -0500

    add data

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NewFoodItem from "./components/NewFoodItem";
 import FoodList from "./components/FoodList";
 import Navbar from "./components/Navbar";
 import Modal
  from "./components/Modal";
+const queryClient = new QueryClient();
 const App = () => {
   const [page, setPage] = useState(true);
   const [open, setOpen] = useState(false);
   const openModal = () => setOpen(true);
   const closeModal = () => setOpen(false);
+  
+
   return (
-    <div>
-      <Navbar title={"Waste Be Gone"}/>
-      <FoodList page={1}/>
-      <button onClick={openModal}>Add item</button>
-      <Modal open={open} close={closeModal}>
-        <NewFoodItem />
-      </Modal>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Navbar title={"Waste Be Gone"}/>
+        <FoodList page={1}/>
+        <button onClick={openModal}>Add item</button>
+        <Modal open={open} close={closeModal}>
+          <NewFoodItem />
+        </Modal>
+      </div>
+    </QueryClientProvider>
   );
 };
 
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,7 +1,22 @@
 import FoodItem from "./FoodItem";
+import { useQuery } from '@tanstack/react-query';
+
+const fetchJson = async(url) => {
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return response.json();
+};
+export const useJsonQuery = (url) => {
+  const { data, isLoading, error } = useQuery([url], () => fetchJson(url));
+  return [ data, isLoading, error ];
+};
+
 
 const NewFoodPage = ({ page }) => {
+  const [data, isLoading, error] = useJsonQuery("./data.json");
+  console.log(data);
   return (
+    // data = useJsonQuery("./data.json");
     <div>
       <FoodItem name={"Tomato"} expires={"2022-10-07"}/>
       <FoodItem name={"Lettuce"} expires={"2022-10-07"}/>
